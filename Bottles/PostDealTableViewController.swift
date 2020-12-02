bbbc0bc115dd4cb01c3eba7c6a3b56dec0649e75
talal ahmad <talalahmad321@gmail.com>
2020-12-03 00:57:55 +0500

    saved data succesffully

--- Bottles/PostDealTableViewController.swift
+++ Bottles/PostDealTableViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import Firebase
+import MBProgressHUD
 class PostDealTableViewController: UITableViewController {
     @IBOutlet weak var dealUrlTF: UITextField!
     @IBOutlet weak var brandTF: UITextField!
@@ -29,7 +30,33 @@ class PostDealTableViewController: UITableViewController {
         brandImageView.image = nil
     }
     @IBAction func submitButtonAction(_ sender: Any) {
+        self.postData(dealurl: dealUrlTF.text ?? "", brandName: brandTF.text ?? "", ItemName: itemTF.text ?? "", originalPrice: originalPriceTF.text ?? "", currentPrice: currentPriceTF.text ?? "", imageUrl: "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
         
     }
-    
+    func postData(dealurl:String,brandName:String,ItemName:String,originalPrice:String,currentPrice:String,imageUrl:String){
+        MBProgressHUD.showAdded(to: view, animated: true)
+        // Add a second document with a generated ID.
+        var ref: DocumentReference? = nil
+
+        ref = db.collection("latest_deals").addDocument(data: [
+            "brand": brandName,
+            "image": imageUrl,
+            "name": ItemName,
+            "new_price": Int(currentPrice) ?? 0,
+            "orignal_price": Int(originalPrice) ?? 0,
+            "url": dealurl,
+            "website":dealurl,
+            "created_at":Date()
+        ]) { err in
+            if let err = err {
+                MBProgressHUD.hide(for: self.view, animated: true)
+                print("Error adding document: \(err)")
+            } else {
+                MBProgressHUD.hide(for: self.view, animated: true)
+                self.clearAll()
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        
+    }
 }
